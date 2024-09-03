8d10699d8dcb864ed87b3b86b10a8d78defac1bb
kamrulkoche <kamrulkoche@gmail.com>
2024-09-03 23:59:38 +0600

    handleRegister

---
+++ src/Register/Register.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Register = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email,password);
+    
+  };
+  return (
+    <div className="">
+      <div className="mx-auto w-1/2">
+        <h2 className="text-3xl mb-4">Please Register</h2>
+        <form onSubmit={handleRegister}>
+          <input
+            className="mb-4 w-3/4 py-2 px-4 border border-black"
+            placeholder="Your Email Address"
+            type="email"
+            name="email"
+            id=""
+          />
+          <br />
+          <input
+            className="mb-4 w-3/4 py-2 px-4 border border-black"
+            placeholder="password"
+            type="password"
+            name="password"
+            id=""
+          />
+          <br />
+          <input
+            className="btn btn-secondary mb-4 w-3/4"
+            type="submit"
+            value="Register"
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/Root/Root.jsx";
+import Home from "./Home/Home.jsx";
+import Login from "./Login/Login.jsx";
+import Register from "./Register/Register.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    children:[
+      {
+        path:'/',
+        element:<Home/>
+      },
+      {
+        path:'/login',
+        element:<Login/>
+      },
+      {
+        path:'/register',
+        element:<Register/>
+      },
+
+    ]
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
